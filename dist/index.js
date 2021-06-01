78618fbc2ccef9917f5cf34fb7a4818cd7bf105c
hristobakalov <hristobak@gmail.com>
2021-06-01 18:05:55 +0200

    missed to build

--- dist/index.js
+++ dist/index.js
@@ -3,24 +3,9 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-Object.defineProperty(exports, "Button", {
-  enumerable: true,
-  get: function get() {
-    return _Button.Button;
-  }
-});
-Object.defineProperty(exports, "Header", {
-  enumerable: true,
-  get: function get() {
-    return _Header.Header;
-  }
-});
-Object.defineProperty(exports, "Page", {
-  enumerable: true,
-  get: function get() {
-    return _Page.Page;
-  }
-});
+var _exportNames = {
+  global: true
+};
 exports.global = void 0;
 
 var global = _interopRequireWildcard(require("./shared/global"));
@@ -29,9 +14,45 @@ exports.global = global;
 
 var _Button = require("./stories/Button");
 
+Object.keys(_Button).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (Object.prototype.hasOwnProperty.call(_exportNames, key)) return;
+  if (key in exports && exports[key] === _Button[key]) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _Button[key];
+    }
+  });
+});
+
 var _Header = require("./stories/Header");
 
+Object.keys(_Header).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (Object.prototype.hasOwnProperty.call(_exportNames, key)) return;
+  if (key in exports && exports[key] === _Header[key]) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _Header[key];
+    }
+  });
+});
+
 var _Page = require("./stories/Page");
+
+Object.keys(_Page).forEach(function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (Object.prototype.hasOwnProperty.call(_exportNames, key)) return;
+  if (key in exports && exports[key] === _Page[key]) return;
+  Object.defineProperty(exports, key, {
+    enumerable: true,
+    get: function get() {
+      return _Page[key];
+    }
+  });
+});
 
 function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
